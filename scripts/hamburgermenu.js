ac737af52e842164618dbde8252e24a54d51f60c
Aaron Lin <lin.aaron1@gmail.com>
2025-11-16 21:12:59 -0800

    Enable language toggling on mobile hamburger bar too

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -1,15 +1,18 @@
-const navToggle = document.getElementById("nav-toggle-button");
-const overlay = document.getElementById("mobile-menu-overlay");
+import { waitForElement } from "./common.js"
 
-navToggle.addEventListener("click", () => {
-  const isActive = overlay.classList.toggle("active");
-  navToggle.textContent = isActive ? "×" : "☰";
-});
+waitForElement("nav-toggle-button", (navToggle) => {
+  const overlay = document.getElementById("mobile-menu-overlay");
 
-overlay.addEventListener("click", (e) => {
-  if (e.target === overlay) {
-    // only if clicking background overlay closes menu
-    overlay.classList.remove("active");
-    navToggle.textContent = "☰";
-  }
+  navToggle.addEventListener("click", () => {
+    const isActive = overlay.classList.toggle("active");
+    navToggle.textContent = isActive ? "×" : "☰";
+  });
+
+  overlay.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+      // only if clicking background overlay closes menu
+      overlay.classList.remove("active");
+      navToggle.textContent = "☰";
+    }
+  });
 });
