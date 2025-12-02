e754ce1ec40a103de9baaa64d0a8974e7725e466
Nancy <nzzuo@andrew.cmu.edu>
2025-12-01 20:42:44 -0700

    changed team page

--- scripts/glslShader.js
+++ scripts/glslShader.js
@@ -5,7 +5,10 @@ async function loadShaderSource(url) {
   return response.text();
 }
 
-async function initShader(canvasId = "skyCanvas", shaderUrl = "../scripts/shader.frag") {
+async function initShader(
+  canvasId = "skyCanvas",
+  shaderUrl = "../scripts/shader.frag"
+) {
   const canvas = document.getElementById(canvasId);
   if (!canvas) {
     console.error(`Canvas with id '${canvasId}' not found.`);
